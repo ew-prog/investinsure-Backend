b19956f9420031f05747830ad273ac1282385c5c
ew-prog <ewanyama@live.co.uk>
2025-10-26 05:00:06 -0700

    Create Agent.js

---
+++ models/Agent.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const agentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Agent", agentSchema);
